15d7a2175502203096f79c1ff4497036a5ff763f
Amrutha Goje <asdflkj.amrutha@gmail.com>
2026-01-02 12:39:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5600342841-fe8eb",
+  "appId": "1:976934826754:web:e6fdfc041249a871ebc81b",
+  "apiKey": "AIzaSyD2Yb237WfRt3gFVYMGxLKPQzVzNIRouTs",
+  "authDomain": "studio-5600342841-fe8eb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "976934826754"
+};
